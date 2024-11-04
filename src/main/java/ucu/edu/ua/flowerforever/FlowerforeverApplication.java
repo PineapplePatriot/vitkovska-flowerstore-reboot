be827c005cf34c5be220db4b90e81d8cd5397d75
Anna-Mariia <vitkovska.pn@ucu.edu.ua>
2024-11-04 21:19:35 +0200

    Minor style fixes

--- src/main/java/ucu/edu/ua/flowerforever/FlowerforeverApplication.java
+++ src/main/java/ucu/edu/ua/flowerforever/FlowerforeverApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlowerforeverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlowerforeverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerforeverApplication.class, args);
+    }
 
 }
--- src/test/java/ucu/edu/ua/flowerforever/FlowerforeverApplicationTests.java
+++ src/test/java/ucu/edu/ua/flowerforever/FlowerforeverApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FlowerforeverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
