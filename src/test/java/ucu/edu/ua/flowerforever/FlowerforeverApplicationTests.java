26892f8261a24e4af294e2278f16600a143eaf4f
Anna-Mariia <vitkovska.pn@ucu.edu.ua>
2024-11-11 21:15:56 +0200

    Another test attempt

--- src/test/java/ucu/edu/ua/flowerforever/FlowerforeverApplicationTests.java
+++ src/test/java/ucu/edu/ua/flowerforever/FlowerforeverApplicationTests.java
@@ -2,8 +2,10 @@ package ucu.edu.ua.flowerforever;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlowerforeverApplicationTests {
 
     @Test
